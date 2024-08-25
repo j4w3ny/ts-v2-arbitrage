5bf7c1f171b74e60f62887dd413f4abe5b947e6c
Jerry Wong <yuki.n@tuta.io>
2024-08-25 08:48:25 +0800

    add deps

--- src/TimeswapV2Arbitrage.sol
+++ src/TimeswapV2Arbitrage.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@uniswap/v3-core/contracts/libraries/LowGasSafeMath.sol";
+
 contract Counter {
+    using LowGasSafeMath for uint256;
+    using LowGasSafeMath for int256;
+
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
