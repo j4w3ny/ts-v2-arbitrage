f4b5d6559ba82e934206493b710487ecf8b27b9a
Jerry Wong <yuki.n@tuta.io>
2024-08-25 21:35:37 +0800

    update deps

--- src/TimeswapV2Arbitrage.sol
+++ src/TimeswapV2Arbitrage.sol
@@ -2,14 +2,23 @@
 pragma solidity ^0.8.13;
 
 import "@uniswap/v3-core/contracts/libraries/LowGasSafeMath.sol";
+import "@uniswap/v3-periphery/contracts/base/PeripheryPayments.sol";
+import '@uniswap/v3-periphery/contracts/base/PeripheryImmutableState.sol';
+import '@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol';
+import '@uniswap/v3-periphery/contracts/libraries/CallbackValidation.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 
-contract Counter {
+
+contract TimeswapV2Arbitrage is IUniswapV3FlashCallback, PeripheryPayments {
     using LowGasSafeMath for uint256;
     using LowGasSafeMath for int256;
 
     uint256 public number;
+	ISwapRouter public immutable swapRouter;
 
     function setNumber(uint256 newNumber) public {
+	
         number = newNumber;
     }
 
