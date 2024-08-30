d1ae1fda72836dd7b3ed3b5e94ebd58feae8346e
Jerry Wong <yuki.n@tuta.io>
2024-08-30 11:44:52 +0800

    done

--- src/TimeswapV2Arbitrage.sol
+++ src/TimeswapV2Arbitrage.sol
@@ -4,13 +4,15 @@ pragma abicoder v2;
 
 import "./interface/ITimeswapV2Pool.sol";
 import "./interface/ITimeswapV2Option.sol";
-import {TimeswapV2OptionBurnCallbackParam} from "./structs/OptionCallbackParam.sol";
+import {TimeswapV2OptionBurnCallbackParam, TimeswapV2OptionMintCallbackParam} from "./structs/OptionCallbackParam.sol";
 import "./structs/PoolParam.sol";
 import {TimeswapV2PoolRebalanceCallbackParam} from "./structs/CallbackParam.sol";
 import "./structs/ArbitrageParam.sol";
 import {TimeswapV2OptionPosition} from "./structs/enums/Position.sol";
 import {ITimeswapV2PoolRebalanceCallback} from "./interface/callback/ITimeswapV2PoolRebalanceCallback.sol";
 import {ITimeswapV2OptionBurnCallback} from "./interface/callback/ITimeswapV2OptionBurnCallback.sol";
+import {ITimeswapV2OptionMintCallback} from "./interface/callback/ITimeswapV2OptionMintCallback.sol";
+
 import {TimeswapV2OptionBurnParam, TimeswapV2OptionMintParam} from "./structs/OptionParam.sol";
 import {TimeswapV2OptionBurn, TimeswapV2OptionMint} from "./structs/enums/OptionTransaction.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -29,6 +31,7 @@ contract TimeswapV2Arbitrage is
     IUniswapV3FlashCallback,
     ITimeswapV2PoolRebalanceCallback,
     Ownable,
+    ITimeswapV2OptionMintCallback,
     ITimeswapV2OptionBurnCallback
 {
     using LowGasSafeMath for uint256;
@@ -44,6 +47,22 @@ contract TimeswapV2Arbitrage is
 
     function uniswapV3FlashCallback(uint256 fee0, uint256 fee1, bytes calldata data) external override {}
 
+    function timeswapV2OptionMintCallback(TimeswapV2OptionMintCallbackParam calldata param)
+        external
+        override
+        returns (bytes memory data)
+    {
+        address option = timeswapV2Pool.optionPair();
+        require(msg.sender == option, "Not allowed external");
+        address token0 = ITimeswapV2Option(option).token0();
+        address token1 = ITimeswapV2Option(option).token1();
+        if (param.token0AndLong0Amount != 0) {
+            TransferHelper.safeTransfer(token0, option, param.token0AndLong0Amount);
+        } else if (param.token1AndLong1Amount != 0) {
+            TransferHelper.safeTransfer(token1, option, param.token1AndLong1Amount);
+        }
+    }
+
     // @dev mint short by flash swap to ensure it pass.
 
     function timeswapV2OptionBurnCallback(TimeswapV2OptionBurnCallbackParam calldata param)
@@ -51,34 +70,58 @@ contract TimeswapV2Arbitrage is
         override
         returns (bytes memory data)
     {
-        require(msg.sender == timeswapV2Pool.optionPair(), "Not allowed external");
-        PoolAddress.PoolKey memory poolKey =
-            PoolAddress.PoolKey({token0: params.token0, token1: params.token1, fee: params.fee1});
-        IUniswapV3Pool pool = IUniswapV3Pool(PoolAddress.computeAddress(factory, poolKey));
+        address option = timeswapV2Pool.optionPair();
+        TimeswapV2PoolRebalanceCallbackParam memory decoded =
+            abi.decode(param.data, (TimeswapV2PoolRebalanceCallbackParam));
+        require(msg.sender == option, "Not allowed external");
+        address token0 = ITimeswapV2Option(option).token0();
+        address token1 = ITimeswapV2Option(option).token1();
+        uint24 poolFee = 500;
+        uint256 deadline = block.timestamp + 200;
+
         if (param.token0AndLong0Amount != 0) {
-            pool.flash(address(this), param.token0AndLong0Amount, 0, bytes(""));
-            // abi.encode(
-            //     FlashCallbackData({
-            //         amount0: param.token0AndLong0Amount,
-            //         amount1: 0,
-            //         payer: msg.sender,
-            //         poolKey: poolKey,
-            //         poolFee2: params.fee2,
-            //         poolFee3: params.fee3
-            //     })
-            // )
+            uint256 token0Amount = IERC20(token0).balanceOf(address(this));
+            TransferHelper.safeApprove(token0, address(swapRouter), token0Amount);
+            ISwapRouter.ExactInputSingleParams memory swapParam = ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(token0),
+                tokenOut: address(token1),
+                fee: poolFee,
+                recipient: address(this),
+                deadline: deadline,
+                amountIn: token0Amount,
+                // @todo add a profit margin here?
+                amountOutMinimum: decoded.long1Amount,
+                sqrtPriceLimitX96: 0
+            });
+
+            swapRouter.exactInputSingle(swapParam);
+            ITimeswapV2Option(option).mint(
+                TimeswapV2OptionMintParam({
+                    strike: param.strike,
+                    maturity: param.maturity,
+                    long0To: address(this),
+                    long1To: address(this),
+                    shortTo: address(this),
+                    transaction: TimeswapV2OptionMint.GivenTokensAndLongs,
+                    amount0: 0,
+                    amount1: decoded.long1Amount,
+                    data: bytes("")
+                })
+            );
         } else if (param.token1AndLong1Amount != 0) {
-            pool.flash(address(this), 0, param.token1AndLong1Amount, bytes(""));
-            // abi.encode(
-            //     FlashCallbackData({
-            //         amount0: param.token0AndLong0Amount,
-            //         amount1: 0,
-            //         payer: msg.sender,
-            //         poolKey: poolKey,
-            //         poolFee2: params.fee2,
-            //         poolFee3: params.fee3
-            //     })
-            // )
+            uint256 token1Amount = IERC20(token1).balanceOf(address(this));
+            TransferHelper.safeApprove(address(token1), address(swapRouter), token1Amount);
+            ISwapRouter.ExactInputSingleParams memory swapParam = ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(token1),
+                tokenOut: address(token0),
+                fee: poolFee,
+                recipient: address(this),
+                deadline: deadline,
+                amountIn: token1Amount,
+                amountOutMinimum: decoded.long0Amount,
+                sqrtPriceLimitX96: 0
+            });
+            swapRouter.exactInputSingle(swapParam);
         }
     }
     // @dev repay the debt of the pool and store profit
@@ -107,40 +150,29 @@ contract TimeswapV2Arbitrage is
                     transaction: TimeswapV2OptionBurn.GivenTokensAndLongs,
                     amount0: 0,
                     amount1: param.long1Amount,
-                    data: bytes("")
-                })
-            );
-            uint256 token1Amount = token1.balanceOf(address(this));
-            TransferHelper.safeApprove(address(token1), address(swapRouter), token1Amount);
-            ISwapRouter.ExactInputSingleParams memory swapParam = ISwapRouter.ExactInputSingleParams({
-                tokenIn: address(token1),
-                tokenOut: address(token0),
-                fee: poolFee,
-                recipient: msg.sender,
-                deadline: deadline,
-                amountIn: token1Amount,
-                amountOutMinimum: param.long0Amount,
-                sqrtPriceLimitX96: 0
-            });
-            swapRouter.exactInputSingle(swapParam);
-
-            TransferHelper.safeApprove(address(token0), address(option), param.long0Amount);
-            // mint position and payback
-            option.mint(
-                TimeswapV2OptionMintParam({
-                    strike: param.strike,
-                    maturity: param.maturity,
-                    long0To: address(option),
-                    long1To: address(option),
-                    shortTo: address(option),
-                    transaction: TimeswapV2OptionMint.GivenTokensAndLongs,
-                    amount0: param.long0Amount,
-                    amount1: 0,
-                    data: bytes("")
+                    data: abi.encode(
+                        TimeswapV2PoolRebalanceCallbackParam({
+                            strike: param.strike,
+                            maturity: param.maturity,
+                            isLong0ToLong1: param.isLong0ToLong1,
+                            long0Amount: param.long0Amount,
+                            long1Amount: param.long1Amount,
+                            data: bytes("")
+                        })
+                    )
                 })
             );
 
-            TransferHelper.safeTransfer(address(token0), owner(), token0.balanceOf(address(this)));
+            option.positionOf(param.strike, param.maturity, address(this), TimeswapV2OptionPosition.Long0);
+
+            option.positionOf(param.strike, param.maturity, address(this), TimeswapV2OptionPosition.Long1);
+
+            option.positionOf(param.strike, param.maturity, address(this), TimeswapV2OptionPosition.Short);
+            IERC20(token0).balanceOf(address(this));
+            IERC20(token1).balanceOf(address(this));
+            option.transferPosition(
+                param.strike, param.maturity, address(timeswapV2Pool), TimeswapV2OptionPosition.Long0, param.long0Amount
+            );
         } else {
             option.burn(
                 TimeswapV2OptionBurnParam({
@@ -151,43 +183,28 @@ contract TimeswapV2Arbitrage is
                     transaction: TimeswapV2OptionBurn.GivenTokensAndLongs,
                     amount0: param.long0Amount,
                     amount1: 0,
-                    data: bytes("MINTSHORT")
+                    data: abi.encode(
+                        TimeswapV2PoolRebalanceCallbackParam({
+                            strike: param.strike,
+                            maturity: param.maturity,
+                            isLong0ToLong1: param.isLong0ToLong1,
+                            long0Amount: param.long0Amount,
+                            long1Amount: param.long1Amount,
+                            data: bytes("")
+                        })
+                    )
                 })
             );
-            uint256 token0Amount = token0.balanceOf(address(this));
-            TransferHelper.safeApprove(address(token0), address(swapRouter), param.long0Amount);
-            ISwapRouter.ExactInputSingleParams memory swapParam = ISwapRouter.ExactInputSingleParams({
-                tokenIn: address(token0),
-                tokenOut: address(token1),
-                fee: poolFee,
-                recipient: msg.sender,
-                deadline: deadline,
-                amountIn: token0Amount,
-                // @todo add a profit margin here?
-                amountOutMinimum: param.long1Amount,
-                sqrtPriceLimitX96: 0
-            });
+            option.positionOf(param.strike, param.maturity, address(this), TimeswapV2OptionPosition.Long0);
 
-            swapRouter.exactInputSingle(swapParam);
+            option.positionOf(param.strike, param.maturity, address(this), TimeswapV2OptionPosition.Long1);
 
-            TransferHelper.safeApprove(address(token1), address(option), param.long1Amount);
-
-            // mint position and payback
-            option.mint(
-                TimeswapV2OptionMintParam({
-                    strike: param.strike,
-                    maturity: param.maturity,
-                    long0To: address(option),
-                    long1To: address(option),
-                    shortTo: address(option),
-                    transaction: TimeswapV2OptionMint.GivenTokensAndLongs,
-                    amount0: 0,
-                    amount1: param.long1Amount,
-                    data: bytes("")
-                })
+            option.positionOf(param.strike, param.maturity, address(this), TimeswapV2OptionPosition.Short);
+            IERC20(token0).balanceOf(address(this));
+            IERC20(token1).balanceOf(address(this));
+            option.transferPosition(
+                param.strike, param.maturity, address(timeswapV2Pool), TimeswapV2OptionPosition.Long1, param.long1Amount
             );
-
-            TransferHelper.safeTransfer(address(token1), owner(), token1.balanceOf(address(this)));
         }
     }
     // @todo make multiple dex source for the arbitrage
@@ -205,5 +222,17 @@ contract TimeswapV2Arbitrage is
                 data: bytes("")
             })
         );
+
+        address option = timeswapV2Pool.optionPair();
+        address token0 = ITimeswapV2Option(option).token0();
+        address token1 = ITimeswapV2Option(option).token1();
+        uint256 token0Balance = IERC20(token0).balanceOf(address(this));
+        uint256 token1Balance = IERC20(token1).balanceOf(address(this));
+        if (token0Balance != 0) {
+            TransferHelper.safeTransfer(address(token0), owner(), token0Balance);
+        }
+        if (token1Balance != 0) {
+            TransferHelper.safeTransfer(address(token1), owner(), token1Balance);
+        }
     }
 }
